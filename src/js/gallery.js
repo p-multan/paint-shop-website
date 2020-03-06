0d2570d6d9badddc7eb836b82a9b18359cb43fc4
Pawel Multan <pj.multan@gmail.com>
2020-03-06 19:28:19 +0100

    improve user experience in gallery section

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,6 +3,7 @@ import 'hammerjs';
 export class Gallery {
   constructor() {
     this.photos = document.querySelectorAll('.js-photo');
+    this.photosInfo = document.querySelectorAll('.js-photo-content');
     this.extendPhotos = document.querySelectorAll('.js-enlarge');
     this.extendIcons = document.querySelectorAll('.js-enlarge-icon');
     this.backdrop = document.getElementById('galleryBackdrop');
@@ -19,27 +20,16 @@ export class Gallery {
 
     this.extendPhotosHandler();
     this.extendIconsHandler();
+    this.extendByPhotosInfoHandler();
     this.controlsHandler();
     document.addEventListener('keyup', this.keyHandler.bind(this));
-    hammer.on('swipeleft', () => {
-      const numberPattern = /\d+/g;
-      const currentSrc = this.activePhoto.getAttribute('src');
-      const currentNumber = +currentSrc.match(numberPattern)[0];
-      const newNumber =
-        currentNumber + 1 > this.photosNumber ? 1 : currentNumber + 1;
-      const newSrc = currentSrc.replace(currentNumber, newNumber);
-      this.activePhoto.setAttribute('src', newSrc);
-    });
 
-    hammer.on('swiperight', () => {
-      const numberPattern = /\d+/g;
-      const currentSrc = this.activePhoto.getAttribute('src');
-      const currentNumber = +currentSrc.match(numberPattern)[0];
-      const newNumber =
-        currentNumber - 1 > 0 ? currentNumber - 1 : this.photosNumber;
-      const newSrc = currentSrc.replace(currentNumber, newNumber);
-      this.activePhoto.setAttribute('src', newSrc);
-    });
+    hammer.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+    hammer.on('swipeleft', this.showNextPhoto.bind(this));
+    hammer.on('swiperight', this.showPreviousPhoto.bind(this));
+
+    hammer.on('swipeup', this.backdropClickHandler.bind(this));
+    hammer.on('swipedown', this.backdropClickHandler.bind(this));
   }
 
   backdropClickHandler() {
@@ -53,6 +43,20 @@ export class Gallery {
         const src = e.target
           .closest('.gallery__photos-item-cover-enlarge')
           .previousElementSibling.getAttribute('src');
+        this.backdrop.classList.add('js-active');
+
+        this.activePhoto.setAttribute('src', src);
+        this.centerPhoto.classList.add('js-active');
+      });
+    });
+  }
+
+  extendByPhotosInfoHandler() {
+    this.photosInfo.forEach(photoInfo => {
+      photoInfo.addEventListener('click', e => {
+        const src = e.target
+          .closest('.js-photo-content')
+          .parentElement.previousElementSibling.firstChild.getAttribute('src');
         this.backdrop.classList.add('js-active');
 
         this.activePhoto.setAttribute('src', src);
@@ -75,28 +79,35 @@ export class Gallery {
     });
   }
 
+  showPreviousPhoto() {
+    const numberPattern = /\d+/g;
+    const currentSrc = this.activePhoto.getAttribute('src');
+    const currentNumber = +currentSrc.match(numberPattern)[0];
+    const newNumber =
+      currentNumber - 1 > 0 ? currentNumber - 1 : this.photosNumber;
+    const newSrc = currentSrc.replace(currentNumber, newNumber);
+    this.activePhoto.setAttribute('src', newSrc);
+  }
+
+  showNextPhoto() {
+    const numberPattern = /\d+/g;
+    const currentSrc = this.activePhoto.getAttribute('src');
+    const currentNumber = +currentSrc.match(numberPattern)[0];
+    const newNumber =
+      currentNumber + 1 > this.photosNumber ? 1 : currentNumber + 1;
+    const newSrc = currentSrc.replace(currentNumber, newNumber);
+    this.activePhoto.setAttribute('src', newSrc);
+  }
+
   controlsHandler() {
     this.controls.forEach(control => {
       control.addEventListener('click', e => {
         if (e.target.classList.contains('js-previous')) {
-          const numberPattern = /\d+/g;
-          const currentSrc = this.activePhoto.getAttribute('src');
-          const currentNumber = +currentSrc.match(numberPattern)[0];
-          const newNumber =
-            currentNumber - 1 > 0 ? currentNumber - 1 : this.photosNumber;
-          const newSrc = currentSrc.replace(currentNumber, newNumber);
-          this.activePhoto.setAttribute('src', newSrc);
+          this.showPreviousPhoto();
         } else if (e.target.classList.contains('js-next')) {
-          const numberPattern = /\d+/g;
-          const currentSrc = this.activePhoto.getAttribute('src');
-          const currentNumber = +currentSrc.match(numberPattern)[0];
-          const newNumber =
-            currentNumber + 1 > this.photosNumber ? 1 : currentNumber + 1;
-          const newSrc = currentSrc.replace(currentNumber, newNumber);
-          this.activePhoto.setAttribute('src', newSrc);
+          this.showNextPhoto();
         } else if (e.target.classList.contains('js-close')) {
-          this.backdrop.classList.remove('js-active');
-          this.centerPhoto.classList.remove('js-active');
+          this.backdropClickHandler();
         }
       });
     });
@@ -104,21 +115,9 @@ export class Gallery {
 
   keyHandler(e) {
     if (e.keyCode === 37) {
-      const numberPattern = /\d+/g;
-      const currentSrc = this.activePhoto.getAttribute('src');
-      const currentNumber = +currentSrc.match(numberPattern)[0];
-      const newNumber =
-        currentNumber - 1 > 0 ? currentNumber - 1 : this.photosNumber;
-      const newSrc = currentSrc.replace(currentNumber, newNumber);
-      this.activePhoto.setAttribute('src', newSrc);
+      this.showPreviousPhoto();
     } else if (e.keyCode === 39) {
-      const numberPattern = /\d+/g;
-      const currentSrc = this.activePhoto.getAttribute('src');
-      const currentNumber = +currentSrc.match(numberPattern)[0];
-      const newNumber =
-        currentNumber + 1 > this.photosNumber ? 1 : currentNumber + 1;
-      const newSrc = currentSrc.replace(currentNumber, newNumber);
-      this.activePhoto.setAttribute('src', newSrc);
+      this.showNextPhoto();
     } else if (e.keyCode === 27) {
       this.backdropClickHandler();
     }
